d1b5fc84e5b29e56d1b52a629cf448c26c31c7f0
Oscar Bryant <oscarcbryant@gmail.com>
2022-03-01 18:36:22 +1100

    updating id in db and newNote so it saves input

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,33 @@
-const notes = require('express').Router();
+const router = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 
-notes.get('/', (req, res) => {
+router.get('/', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-notes.post('/', (req, res) => {
+router.post('/', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   console.log(req.body);
 
   const { title, text } = req.body;
 
   if (title && text) {
-    
+
     const newNote = {
       title,
       text,
-      notes_id: uuid(),
+      id: uuid(),
     };
 
 readAndAppend(newNote, './db/db.json');
-res.json(`Tip added successfully 🚀`);
+res.json(`Note added successfully 🚀`);
 } else {
-res.error('Error in adding tip');
+res.error('Error in adding note');
 }
 
 });
 
-module.exports = notes;
+module.exports = router;
